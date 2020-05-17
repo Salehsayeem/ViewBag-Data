1d11f2137d84fe87f86a542383433d59e55c7204
Saleh Sayeem <16103179@iubat.edu>
2020-05-17 13:36:38 +0600

    Messages

---
+++ MyFirstMVCProject/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyFirstMVCProject.Models;
+
+namespace MyFirstMVCProject.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<Customer> cus = new List<Customer>
+            {
+                new Customer
+                {
+                    Id = 1,
+                    Name = "Sayeem",
+                    Address = "Dhaka"
+                }
+            };
+            ViewBag.Customer = cus;
+            return View();
+        }
+    }
+}
---
+++ MyFirstMVCProject/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Message Sent from view data";
+            ViewData["CurrentDate"] = DateTime.Now.ToLongTimeString();
+
+            string[] cars = {"Ford", "Ferrari", "Volvo"};
+            ViewData["Cars"] = cars;
+
+            // ViewBag 
+            ViewBag.Message = "Message Sent from Viewbag";
+            ViewBag.CurrentDateTime = DateTime.Now.ToLongDateString();
+            string[] car = {"BMW", "Audi", "Ferrari"};
+            ViewBag.CarList = car;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
